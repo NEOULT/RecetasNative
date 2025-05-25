8900b14d0b549f93d18e8d2e8d4f0cd74b3940cd
ange1020 <angelina.30571079@uru.edu>
2025-05-25 17:45:07 -0400

    feat: Implementación del componente ImagePicker - Se agregó un nuevo Hook useImagePicker

--- RecetasNative/src/app/(protected)/(tabs)/addButton.js
+++ RecetasNative/src/app/(protected)/(tabs)/addButton.js
@@ -1,11 +1,13 @@
 import { StyleSheet, View, Text, TextInput} from 'react-native';
 import IngredientItem from '../../../components/IngredientItem';
+import ImageSelector from '../../../components/common/ImagePicker';
 
 
 export default function AddScreen() {
 
   return (
     <View style={styles.container}>
+        <ImageSelector width={200} height={100}/>
         <IngredientItem/>
         <IngredientItem/>
         <IngredientItem/>
---
+++ RecetasNative/src/components/common/ImagePicker.js
@@ -0,0 +1,60 @@
+import { View, Image, StyleSheet, TouchableOpacity, Alert, Text } from "react-native";
+import { MaterialIcons } from "@expo/vector-icons";
+import { useImagePicker } from "../../hooks/useImagePicker";
+
+export default function ImageSelector({width = 100, height = 100}) {
+    const { imageUri, setImageUri, pickImage, takePhoto } = useImagePicker();
+
+    const handlePress = () => {
+        if (imageUri) {
+        Alert.alert("Imagen seleccionada", "¿Qué deseas hacer?", [
+            { text: "Eliminar", onPress: () => setImageUri(null), style: "destructive" },
+            { text: "Cambiar imagen", onPress: showPickerOptions },
+            { text: "Cancelar", style: "cancel" },
+        ]);
+        } else {
+        showPickerOptions();
+        }
+    };
+
+    const showPickerOptions = () => {
+        Alert.alert("Seleccionar imagen", "¿Qué deseas hacer?", [
+        { text: "Cancelar", style: "cancel" },
+        { text: "Cámara", onPress: takePhoto },
+        { text: "Galería", onPress: pickImage },
+        ]);
+    };
+
+    return (
+        <TouchableOpacity style={[styles.square, {width, height}]} onPress={handlePress}>
+        {imageUri ? (
+            <Image source={{ uri: imageUri }} style={styles.image} />
+        ) : (
+            <View style={styles.placeholder}>
+            <MaterialIcons name="photo-camera" size={40} color="#999" />
+            <Text style={{ color: "#999", marginTop: 5 }}>Agregar foto</Text>
+            </View>
+        )}
+        </TouchableOpacity>
+    );
+    }
+
+const styles = StyleSheet.create({
+    square: {
+        backgroundColor: "#eee",
+        borderRadius: 10,
+        justifyContent: "center",
+        alignItems: "center",
+        borderWidth: 1,
+        borderColor: "#ccc",
+        overflow: "hidden",
+    },
+    placeholder: {
+        justifyContent: "center",
+        alignItems: "center",
+    },
+    image: {
+        width: "100%",
+        height: "100%",
+    },
+});
